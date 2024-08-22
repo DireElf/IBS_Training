a9d70f347a7b68a408e10d18906ebb007735a686
DireElf <3672@mail.ru>
2024-08-22 09:15:56 +0300

    optimize sql queries

--- src/main/java/com/ibs/db/utils/Queries.java
+++ src/main/java/com/ibs/db/utils/Queries.java
@@ -1,9 +1,10 @@
 package com.ibs.db.utils;
 
 public class Queries {
-    public static final String SELECT_ALL = "SELECT * FROM FOOD";
+    public static final String SELECT_ALL = "SELECT FOOD_ID, FOOD_NAME, FOOD_TYPE, FOOD_EXOTIC FROM FOOD";
     public static final String SELECT_ALL_ENTRIES_COUNT = "SELECT COUNT(*) FROM FOOD";
-    public static final String SELECT_ENTRY_BY_ID = "SELECT * FROM FOOD WHERE FOOD_ID = ?";
+    public static final String SELECT_ENTRY_BY_ID
+            = "SELECT FOOD_ID, FOOD_NAME, FOOD_TYPE, FOOD_EXOTIC FROM FOOD WHERE FOOD_ID = ?";
     public static final String ADD_NEW_GOOD
             = "INSERT INTO FOOD(FOOD_NAME, FOOD_TYPE, FOOD_EXOTIC) VALUES (?, ?, ?)";
     public static final String DELETE_ENTRY_BY_ID = "DELETE FROM FOOD WHERE FOOD_ID = ?";
