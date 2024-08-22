b33d53fa57a5099638530b45b1cc2826a47f4ec1
DireElf <3672@mail.ru>
2024-08-22 20:53:28 +0300

    fix window size

--- src/test/java/com/ibs/tests/cucumber/steps/Hooks.java
+++ src/test/java/com/ibs/tests/cucumber/steps/Hooks.java
@@ -4,12 +4,18 @@ import com.ibs.managers.DriverManager;
 import com.ibs.managers.PropManager;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
 
 import static com.ibs.utils.PropConst.TEST_APP_URL;
 
 public class Hooks {
     private static final PropManager propManager = PropManager.getPropManager();
     private static final DriverManager driverManager = DriverManager.getDriverManager();
+
+    @BeforeAll
+    public static void initDriver() {
+        driverManager.getWebDriver().manage().window().maximize();
+    }
 
     @Before(value = "@test-by-UI")
     public static void openHomePage() {
